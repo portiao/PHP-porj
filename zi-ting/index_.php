f6ed5d0aeeb0f4c37d3e509eba630fa2d67e9d67
Portia Chen <portiachen81@gmail.com>
2021-09-04 14:08:39 +0800

    梓庭PHP 會員登入註冊

---
+++ zi-ting/index_.php
@@ -0,0 +1,22 @@
+<?php
+include __DIR__ . '/partials/init.php';
+$title = 'C組php&sql小專';
+?>
+<?php include __DIR__ . '/partials/html-head.php'; ?>
+<?php include __DIR__ . '/partials/navbar.php'; ?>
+<div class="container">
+    
+    <div class="pic">
+        <img src="./img/3.jpg" alt="">.
+        
+    </div>
+
+    <style>
+        .pic{
+            margin-top: 80px;
+        
+        }
+    </style>
+</div>
+<?php include __DIR__ . '/partials/scripts.php'; ?>
+<?php include __DIR__ . '/partials/html-foot.php'; ?>
